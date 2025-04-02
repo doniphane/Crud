6ff7af5138d25e1aad723cd5874c613a549d484e
doniphane <138102610+doniphane@users.noreply.github.com>
2025-04-02 10:52:39 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Crud
+# Cette application permet à un utilisateur de gérer une liste de tâches. Il peut :
+#  Ajouter une nouvelle tâche
+# Supprimer une tâche
+ # Sauvegarder les tâches dans le navigateur (localStorage) fonctionalité en cours réalisation
+# L'application est développée avec HTML, CSS et JavaScript.
--- script.js
+++ script.js
@@ -1,25 +1,41 @@
+// Fonction pour ajouter une tâche à la liste
 function addTask() {
+    // Récupère l'élément input où l'utilisateur qui est sa tache 
     let taskInput = document.getElementById("taskInput");
+
+    // Récupère la valeur entrée et supprime les espaces 
     let taskText = taskInput.value.trim();
 
+    // Vérifie si l'utilisateur n'a rien entré
     if (taskText === "") {
-        alert("Veuillez entrer une tâche !");
+        alert("Veuillez entrer une tâche !"); 
         return;
     }
 
+    // Récupère l'élément et  affichées les tâches
     let taskList = document.getElementById("taskList");
 
+    // Crée un nouvel élément pour la tâche
     let li = document.createElement("li");
+
+    // Définit le contenu de la tâche avec un bouton de suppression
     li.innerHTML = `
-        ${taskText}
+        ${taskText} 
         <button class="delete-btn" onclick="removeTask(this)">Supprimer</button>
     `;
 
+    // Ajoute la nouvelle tâche à la liste des tâches
     taskList.appendChild(li);
+
+
     taskInput.value = "";
 }
 
+// Fonction pour supprimer une tâche
 function removeTask(button) {
+    // Récupère l'élément parent du bouton cliqué 
     let li = button.parentElement;
+
+    // Supprime l'élément  de la listees
     li.remove();
 }
